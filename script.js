d6f773e4c82c95a22dffa648e26b09b7322b41f9
Inna T <meeoow005@gmail.com>
2022-09-06 18:11:33 +0400

    init

---
+++ script.js
@@ -0,0 +1,52 @@
+document.addEventListener("keydown", function (event) {
+    if (event.code == "KeyA") {
+        let audio = new Audio("./sounds/white-keys-sounds/A.mp3");
+        audio.play();
+    }
+    if (event.code == "KeyD") {
+        let audio = new Audio("./sounds/white-keys-sounds/D.mp3");
+        audio.play();
+    }
+    if (event.code == "KeyF") {
+        let audio = new Audio("./sounds/white-keys-sounds/F.mp3");
+        audio.play();
+    }
+    if (event.code == "KeyG") {
+        let audio = new Audio("./sounds/white-keys-sounds/G.mp3");
+        audio.play();
+    }
+    if (event.code == "KeyH") {
+        let audio = new Audio("./sounds/white-keys-sounds/H.mp3");
+        audio.play();
+    }
+    if (event.code == "KeyJ") {
+        let audio = new Audio("./sounds/white-keys-sounds/J.mp3");
+        audio.play();
+    }
+    if (event.code == "KeyS") {
+        let audio = new Audio("./sounds/white-keys-sounds/S.mp3");
+        audio.play();
+    }
+    if (event.code == "KeyE") {
+        let audio = new Audio("./sounds/black-keys-sounds/E.mp3");
+        audio.play();
+    }
+    if (event.code == "KeyT") {
+        let audio = new Audio("./sounds/black-keys-sounds/T.mp3");
+        audio.play();
+    }
+    if (event.code == "KeyU") {
+        let audio = new Audio("./sounds/black-keys-sounds/U.mp3");
+        audio.play();
+    }
+    if (event.code == "KeyW") {
+        let audio = new Audio("./sounds/black-keys-sounds/W.mp3");
+        audio.play();
+    }
+    if (event.code == "KeyY") {
+        let audio = new Audio("./sounds/black-keys-sounds/Y.mp3");
+        audio.play();
+    }
+});
+
+
